138b63da9960a2e467e6104e47f3c9f58eb47b50
Sebastian Rodriguez <sebaser99@gmail.com>
2021-01-27 11:32:07 -0500

    proyecto finalizado

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
@@ -28,6 +29,15 @@ module.exports = {
                 template: './public/index.html',
                 title: "Cientifico"
             }
-        )
+        ),
+        new CopyWebpackPlugin({
+            patterns: 
+            [
+                {
+                    from: './src/styles/styles.css',
+                    to: ''
+                } 
+            ],      
+        }),
     ]
-}
+};
